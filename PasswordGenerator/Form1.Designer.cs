67fb1732b5da7785c4f05c9eba082e47f3dea76c
BitKovin <39824091+BitKovin@users.noreply.github.com>
2024-01-31 14:18:55 +0200

    Initial commit

---
+++ PasswordGenerator/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace PasswordGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(72, 106);
+            label1.Name = "label1";
+            label1.Size = new Size(102, 25);
+            label1.TabIndex = 0;
+            label1.Text = "use capital";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(54, 141);
+            label2.Name = "label2";
+            label2.Size = new Size(120, 25);
+            label2.TabIndex = 1;
+            label2.Text = "use numbers";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(7, 183);
+            label3.Name = "label3";
+            label3.Size = new Size(177, 25);
+            label3.TabIndex = 2;
+            label3.Text = "use special symbols";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(36, 39);
+            label4.Name = "label4";
+            label4.Size = new Size(66, 25);
+            label4.TabIndex = 3;
+            label4.Text = "length";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(108, 39);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "6";
+            textBox1.Size = new Size(97, 29);
+            textBox1.TabIndex = 4;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(190, 192);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(15, 14);
+            checkBox1.TabIndex = 5;
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(190, 150);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(15, 14);
+            checkBox2.TabIndex = 6;
+            checkBox2.UseVisualStyleBackColor = true;
+            checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(190, 115);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(15, 14);
+            checkBox3.TabIndex = 7;
+            checkBox3.UseVisualStyleBackColor = true;
+            checkBox3.CheckedChanged += checkBox3_CheckedChanged;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(108, 246);
+            button1.Name = "button1";
+            button1.Size = new Size(117, 40);
+            button1.TabIndex = 8;
+            button1.Text = "Generate";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(54, 292);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(223, 34);
+            textBox2.TabIndex = 9;
+            // 
+            // button2
+            // 
+            button2.Enabled = false;
+            button2.Location = new Point(118, 369);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 10;
+            button2.Text = "save to file";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(324, 404);
+            Controls.Add(button2);
+            Controls.Add(textBox2);
+            Controls.Add(button1);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            Text = "password generator";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private Button button1;
+        private TextBox textBox2;
+        private Button button2;
+    }
+}
---
+++ PasswordGenerator/Form1.cs
@@ -0,0 +1,185 @@
+namespace PasswordGenerator
+{
+    public partial class Form1 : Form
+    {
+
+        int length = 6;
+
+        bool capital = false;
+
+        bool useNumber = false;
+
+        bool useSpecial = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int old = length;
+
+
+            try
+            {
+                length = int.Parse(textBox1.Text);
+
+                if (length <= 0)
+                {
+                    length = old;
+                    textBox1.Text = old.ToString();
+                    MessageBox.Show("password must be at least 1 symbol long");
+                }
+
+            }
+            catch
+            {
+                textBox1.Text = old.ToString();
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            capital = checkBox3.Checked;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            useSpecial = checkBox1.Checked;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            useNumber = checkBox2.Checked;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Generate();
+        }
+
+        void Generate()
+        {
+            List<string> chars = new List<string>();
+
+            List<string> lower = new List<string>() { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
+            List<string> upper = new List<string>() { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+            List<string> nums = new List<string>() { "1", "2", "3", "4", "5", "6", "7", "8", "8" };
+
+            List<string> symbols = new List<string>() { "!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "=", "~", "`", "|", "", "/", "(", ")", "{", "}", "[", "]", ":", ";", "'", ",", ".", "<", ">", "?" };
+
+            chars.AddRange(lower);
+
+            if (capital)
+                chars.AddRange(upper);
+
+            if (useNumber)
+                chars.AddRange(nums);
+
+            if (useSpecial)
+                chars.AddRange(symbols);
+
+            string pass = "";
+
+            Random rand = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int n = rand.Next(chars.Count - 1);
+
+                pass = pass + chars[n];
+            }
+
+            if (ContainsElement(pass, lower) == false)
+            {
+                Console.WriteLine("requred lower symbol not found");
+                Generate();
+                return;
+            }
+
+
+            if (capital)
+                if (ContainsElement(pass, upper) == false)
+                {
+                    Console.WriteLine("requred capital symbol not found");
+                    Generate();
+                    return;
+                }
+
+
+            if (useNumber)
+                if (ContainsElement(pass, nums) == false)
+                {
+                    Console.WriteLine("requred number not found");
+                    Generate();
+                    return;
+                }
+
+
+            if (useSpecial)
+                if (ContainsElement(pass, symbols) == false)
+                {
+                    Console.WriteLine("requred symbol not found");
+                    Generate();
+                    return;
+                }
+
+            textBox2.Text = pass;
+            button2.Enabled = true;
+        }
+
+        bool ContainsElement(string arr, List<string> elements)
+        {
+            foreach (string element in elements)
+            {
+                if (arr.Contains(element))
+                    return true;
+            }
+            return false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        void Save()
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "Text Files|*.txt|All Files|*.*";
+            saveFileDialog1.Title = "Save a Password File";
+            saveFileDialog1.FileName = "password.txt";
+
+            DialogResult result = saveFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                // Save the content to the selected file
+                string filePath = saveFileDialog1.FileName;
+                SaveContentToFile(filePath, textBox2.Text);
+                Console.WriteLine("File saved successfully at: " + filePath);
+            }
+        }
+
+        void SaveContentToFile(string filePath, string content)
+        {
+
+            try
+            {
+                File.WriteAllText(filePath, content);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error saving file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+    }
+}
